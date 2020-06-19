2d919266c004e3d97580b92031e5d0683a6301b5
fakeboyas <ovoivi9@gmail.com>
2020-06-19 10:55:38 +0700

    Add GUI

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ let test1 = "aku belum sarapan";
 
 console.log(test1.toTitleCase());
 
-// let btn1 = document.getElementById("btn1");
-// let text1 = .document.getElementById("inputText")
+let btn1 = document.getElementById("btn1");
 
-// function ubah()
-
-// btn1.addEventListener(clic)
+btn1.addEventListener("click", function () {
+    let text1 = document.getElementById("inputText").value;
+    let display = document.getElementById("display");
+    display.innerHTML = text1.toTitleCase();
+});
