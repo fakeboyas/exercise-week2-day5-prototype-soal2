cf9d728107c841d768d5e4da4544685664f0f402
fakeboyas <ovoivi9@gmail.com>
2020-06-19 11:31:51 +0700

    add alternatif

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
+// String.prototype.toTitleCase = function () {
+//     return this.replace(/\w\S*/g, function (add) {
+//         return add.charAt(0).toUpperCase() + add.substr(1).toLowerCase();
+//     });
+// };
+
 String.prototype.toTitleCase = function () {
-    return this.replace(/\w\S*/g, function (add) {
-        return add.charAt(0).toUpperCase() + add.substr(1).toLowerCase();
-    });
+    return this.split(" ")
+        .map((txt) => txt[0].toUpperCase() + txt.substr(1).toLowerCase())
+        .join(" ");
 };
 
 // let test1 = "aku belum sarapan";
